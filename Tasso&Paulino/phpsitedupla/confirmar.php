86de3121a8a9bd526f5fb0ef6053cf7d940655c7
Tasso67 <202413600044@ifba.edu.br>
2026-09-11 15:48:13 -0300

    Update confirmar.php

--- Tasso&Paulino/phpsitedupla/confirmar.php
+++ Tasso&Paulino/phpsitedupla/confirmar.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 
-$produto = isset($_GET['produto']) ? $_GET['produto'] : "Produto não informado";
-$preco = isset($_GET['preco']) ? $_GET['preco'] : "0";
+$produto = isset($_POST['produto']) ? $_POST['produto'] : "Produto não informado";
+$preco = isset($_POST['preco']) ? $_POST['preco'] : "0";
 ?>
 
 <html>
@@ -10,27 +10,20 @@ $preco = isset($_GET['preco']) ? $_GET['preco'] : "0";
     <title>Compra Confirmada</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
-
 <body>
-
 <div class="header">
     <h1>🎬 Mundo do Cinema</h1>
 </div>
-
 <div class="container">
     <div class="card">
         <h2>✅ Compra realizada!</h2>
-
         <p><strong>Produto:</strong> <?= $produto ?></p>
         <p><strong>Preço:</strong> R$ <?= $preco ?></p>
-
         <br>
-
         <a href="index.php">
             <button>Voltar ao início</button>
         </a>
     </div>
 </div>
-
 </body>
 </html>
