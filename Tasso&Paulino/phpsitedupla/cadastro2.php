2120344b868e60b7d963a4db1786c204d7a3165f
Tasso67 <202413600044@ifba.edu.br>
2026-09-11 15:47:05 -0300

    Update cadastro2.php

--- Tasso&Paulino/phpsitedupla/cadastro2.php
+++ Tasso&Paulino/phpsitedupla/cadastro2.php
@@ -2,25 +2,17 @@
 <head>
     <link rel="stylesheet" href="estilo.css">
 </head>
-
 <body>
-
     <div class="container">
         <div class="card">
-
             <h2>Crie seu login</h2>
-
             <form action="salvar_login.php" method="POST">
                 <input type="text" name="login" placeholder="Login" required><br><br>
                 <input type="password" name="senha" placeholder="Senha" required><br><br>
-
-                <input type="hidden" name="cpf" value="<?= $_GET['cpf'] ?>">
-
-                <button>Cadastrar</button>
+                <input type="hidden" name="cpf" value="<?= $_POST['cpf'] ?? '' ?>">
+                <button type="submit">Cadastrar</button>
             </form>
-
         </div>
     </div>
-
 </body>
 </html>
