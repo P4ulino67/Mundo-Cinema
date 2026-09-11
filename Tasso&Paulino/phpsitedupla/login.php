3de4443ac6ec3cd633b0b6eef2130e6b47cbe19f
Tasso67 <202413600044@ifba.edu.br>
2026-09-11 15:49:32 -0300

    Update login.php

--- Tasso&Paulino/phpsitedupla/login.php
+++ Tasso&Paulino/phpsitedupla/login.php
@@ -1,18 +1,15 @@
 <?php
 session_start();
 ?>
-<html>
 
+<html>
 <head>
     <title>Login - Mundo do Cinema</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
-
 <body>
-
     <div class="header">
         <h1>🎬 Mundo do Cinema</h1>
-
         <nav class="navbar">
             <ul>
                 <li><a href="index.php">Início</a></li>
@@ -20,37 +17,30 @@ session_start();
             </ul>
         </nav>
     </div>
-
     <div class="container">
         <div class="card">
-
             <h2>Entrar na sua conta</h2>
-
             <?php
-            if (isset($_GET['erro'])) {
+            if (isset($_SESSION['erro'])) {
                 echo "<p style='color:red;'>Login inválido!</p>";
+                unset($_SESSION['erro']);
             }
             ?>
 
             <form action="processa_login.php" method="POST">
                 <input type="text" name="login" placeholder="Login" required><br><br>
                 <input type="password" name="senha" placeholder="Senha" required><br><br>
-
-                <input type="hidden" name="produto" value="<?= isset($_GET['produto']) ? $_GET['produto'] : '' ?>">
-                <input type="hidden" name="preco" value="<?= isset($_GET['preco']) ? $_GET['preco'] : '' ?>">
-
+                <input type="hidden" name="produto" value="<?= $_POST['produto'] ?? '' ?>">
+                <input type="hidden" name="preco" value="<?= $_POST['preco'] ?? '' ?>">
                 <button type="submit">Entrar</button>
             </form>
-
             <p>Ainda não tem conta?</p>
             <a href="cadastro1.php">Criar conta</a>
-
         </div>
     </div>
-
     <div class="footer">
         <p>&copy; Tasso Farias e Paulino Mendes</p>
     </div>
-
 </body>
+
 </html>
