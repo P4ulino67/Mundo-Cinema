b510ac3b2f9b7cb272e103c2d53e81f800fbaacb
Tasso67 <202413600044@ifba.edu.br>
2026-09-13 18:50:07 -0300

    Update finalizar.php

--- Mundo-Cinema-main-CORRIGIDO(Tasso e Paulino)/Tasso&Paulino/phpsitedupla/finalizar.php
+++ Mundo-Cinema-main-CORRIGIDO(Tasso e Paulino)/Tasso&Paulino/phpsitedupla/finalizar.php
@@ -10,47 +10,30 @@ $_SESSION['carrinho'] = [];
 ?>
 
 <html>
-
 <head>
     <title>Compra Finalizada</title>
     <link rel="stylesheet" href="css/estilo.css">
 </head>
-
 <body>
-
     <div class="header">
-
         <img src="img/logo-mundo-cinema.jpg" alt="Mundo Cinema" class="logo-site">
-
     </div>
-
     <div class="container">
-
         <div class="section">
-
             <div class="card">
-
                 <h2>✅ Compra Finalizada!</h2>
-
                 <p>
                     Obrigado pela sua compra.
                     Seu pedido foi realizado com sucesso.
                 </p>
-
                 <br>
-
                 <a href="index.php">
                     <button>
                         Voltar para Loja
                     </button>
                 </a>
-
             </div>
-
         </div>
-
     </div>
-
 </body>
-
 </html>
